42b16acf35d4bdecd04834538054fe394425f71c
sagdish <forsi1@yahoo.com>
2018-03-22 13:00:22 -0700

    mini almost done

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import { App } from './components';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-loger';
+
+import { App } from './components';
+import rootReducer from './reducers/rootReducer';
+import './index.css';
+
 // needed dependancies
 // applyMiddleware from redux
 // thunk from redux-thunk
 // logger from redux-logger
 // rootReducer from ./reducers
 
-const store = createStore(/* rootReducer */, /* applyMiddleware goes here */);
+const middleware = applyMiddleware(logger, thunk);
+const store = createStore(rootReducer, middleware);
 
 ReactDOM.render(
   <Provider store={store}>
